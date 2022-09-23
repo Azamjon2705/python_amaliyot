b12a5f5def9ef61f9c7877919f97a2ad1d94f920
A'zamjon Muhammadaliyev <113654665+Azamjon2705@users.noreply.github.com>
2022-09-23 14:18:25 +0300

    Add files via upload

---
+++ 1-amaliy ish.py
@@ -0,0 +1,9 @@
+### 18-misol ###  Muhammadaliyev A'zamjon
+a=int(input('a='))
+b=int(input('b='))
+y=a+b
+x=a-b
+k=a*b
+l=a/b
+print(f'yigindisi {y}\n ayirmasi {x}\n kopaytmasi {k}\n bolinmasi {l}')
+
