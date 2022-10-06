04134ed65591586ac77176533d2bf05e33b5bfa2
A'zamjon Muhammadaliyev <113654665+Azamjon2705@users.noreply.github.com>
2022-10-06 20:33:30 +0300

    Add files via upload

---
+++ 2-amaliy ish.py
@@ -0,0 +1,17 @@
+ #20-misol.		Doirа yuzi S berilgаn.
+ # Uning diаmetri D vа аylаnаsi uzunligi L ni
+ # L=2*pi*R va S=pi*R**2 formulаlаrdаn foydаlаnib toping.
+
+from math import fabs,sqrt,pow
+S=int(input("yuza kirit>>>"))
+R=int(input("radiusni kirit>>>"))
+pi=3.14
+L=2*pi*R
+D=2*R
+print(f"diametri={D}\nuzunligi={L}")
+
+
+
+
+
+
